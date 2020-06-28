de947308f03fb65903c3edf8be0cdd639a5c7709
djx0716 <49354879+djx0716@users.noreply.github.com>
2020-06-28 02:51:08 -0400

    Create my_vm.h

---
+++ Project 3 User level Memory Management/my_vm.h
@@ -0,0 +1,59 @@
+#ifndef MY_VM_H_INCLUDED
+#define MY_VM_H_INCLUDED
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <math.h>
+#include <string.h>
+#include <pthread.h>
+
+//Assume the address space is 32 bits, so the max memory size is 4GB
+//Page size is 4KB
+
+//Add any important includes here which you may need
+
+#define PGSIZE 4096
+
+// Maximum size of virtual memory
+#define MAX_MEMSIZE 4ULL*1024*1024*1024
+
+// Size of "physcial memory"
+#define MEMSIZE 1024*1024*1024
+
+// Represents a page table entry
+typedef unsigned long pte_t;
+
+// Represents a page directory entry
+typedef unsigned long pde_t;
+
+#define TLB_ENTRIES 512
+
+//Structure to represents TLB
+typedef struct tlb{
+    /*Assume your TLB is a direct mapped TLB with number of entries as TLB_ENTRIES
+    * Think about the size of each TLB entry that performs virtual to physical
+    * address translation.
+    */
+    int stored_virtual_num;
+    unsigned long stored_physical_addr;
+}tlb;
+
+
+void set_physical_mem();
+pte_t* translate(pde_t *pgdir, void *va);
+int page_map(pde_t *pgdir, void *va, void* pa);
+int add_TLB(int tag);
+unsigned long check_TLB(int tag);
+void *a_malloc(unsigned int num_bytes);
+void a_free(void *va, int size);
+void put_value(void *va, void *val, int size);
+void get_value(void *va, void *val, int size);
+void mat_mult(void *mat1, void *mat2, int size, void *answer);
+void print_TLB_missrate();
+
+
+/* helper functions */
+unsigned long va_generator(unsigned long outer, unsigned long inner);
+
+#endif
